149ff6e543a7d9d0d28fcd3c42338b46c8cd14cd
sallywoolweaver <sallywoolweaver@gmail.com>
2023-02-02 19:24:29 -0600

    init

---
+++ chatgpt_orgin.py
@@ -0,0 +1,48 @@
+import requests
+from dalleimages import Dalle
+API_KEY = "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"
+PROMPT = "Generate an origin story for a fighter in a RPG game. It should be at least 500 words."
+
+def generate_story():
+    response = requests.post(
+        "https://api.openai.com/v1/engines/text-davinci-002/completions",
+        headers={
+            "Authorization": f"Bearer {API_KEY}",
+            "Content-Type": "application/json",
+        },
+        json={
+            "prompt": PROMPT,
+            "max_tokens": 1024,
+            "n": 1,
+            "stop": None,
+            "temperature": 0.5,
+        },
+    )
+    response_json = response.json()
+    return response_json["choices"][0]["text"]
+
+def summarize_story(story):
+    response = requests.post(
+        "https://api.openai.com/v1/engines/text-davinci-002/completions",
+        headers={
+            "Authorization": f"Bearer {API_KEY}",
+            "Content-Type": "application/json",
+        },
+        json={
+             "prompt": "Summerize in 15 words this story: "+story,
+            "max_tokens": 1024,
+            "n": 1,
+            "stop": None,
+            "temperature": 0.5,
+        }
+    )
+    response_json = response.json()
+    return response_json["choices"][0]["text"]
+
+if __name__ == "__main__":
+    story = generate_story()
+    print(story)
+    summarized_story = summarize_story(story)
+    print("summ" + summarized_story)
+    #Dalle("generate an image of a fighter in an RPG game")
+    Dalle(summarized_story)
---
+++ dalleimages.py
@@ -0,0 +1,49 @@
+import requests
+import cv2
+import numpy as np
+import json
+
+class Dalle:
+    QUERY_URL = "https://api.openai.com/v1/images/generations"
+
+    def generate_image(self, prompt, api_key):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_key
+        }
+
+        model = "image-alpha-001"
+        data = {
+    "model": model,
+    "prompt": prompt,
+    "num_images": 1,
+    "size": "1024x1024",
+    "response_format": "url"
+}
+
+        resp = requests.post(self.QUERY_URL, headers=headers, json=data)
+
+        if resp.status_code != 200:
+            raise ValueError("Failed to generate image "+resp.text)
+
+        response_text = json.loads(resp.text)
+        return response_text['data'][0]['url']
+
+    def display_image(self, image_url):
+        # Read the image from the URL
+        resp = requests.get(image_url)
+        image = np.asarray(bytearray(resp.content), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        # Display the image
+        cv2.imshow("Image", image)
+        # Wait until a key is pressed
+        cv2.waitKey(0)
+        # Destroy the window
+        cv2.destroyAllWindows()
+
+    def __init__(self, pr):
+        api_key = "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"
+        prompt = pr
+        self.QUERY_URL = "https://api.openai.com/v1/images/generations"
+        image_url = self.generate_image(prompt, api_key)
+        self.display_image(image_url)
