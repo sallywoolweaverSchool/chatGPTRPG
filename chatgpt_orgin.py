74cbd3709935c23c08ae5c9e09df5c599b5d7966
sallywoolweaver <sallywoolweaver@gmail.com>
2023-02-02 22:42:19 -0600

    added text to speak

--- chatgpt_orgin.py
+++ chatgpt_orgin.py
@@ -1,7 +1,11 @@
 import requests
+import pyttsx3
+from gtts import gTTS
 from dalleimages import Dalle
+import threading
+import pygame
 API_KEY = "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"
-PROMPT = "Generate an origin story for a fighter in a RPG game. It should be at least 500 words. Give the main character a name."
+PROMPT = "Generate an origin story for a fighter in a RPG game. It should be at least 500 words. Give the main character a name. It should be PG-13"
 
 def generate_story():
     response = requests.post(
@@ -39,10 +43,35 @@ def summarize_story(story):
     response_json = response.json()
     return response_json["choices"][0]["text"]
 
+def save_story(story):
+    with open("story.txt", "w") as file:
+        file.write(story)
+    
+def dis_story(summarized_story):
+    Dalle(summarized_story)   
+    
+def speak_textPYTT(story):
+    engine = pyttsx3.init()
+    engine.say(story)
+    engine.runAndWait()
+    print('finished talking')
+
+def speak_text(story):
+    print("test")
+    tts = gTTS(text=story, lang='en')
+    tts.save("story.mp3")
+    pygame.mixer.init()
+    pygame.mixer.music.load("story.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.wait(1000)
+    print('finished talking')
+    
 if __name__ == "__main__":
     story = generate_story()
     print(story)
     summarized_story = summarize_story(story)
-    print("summ" + summarized_story)
-    #Dalle("generate an image of a fighter in an RPG game")
-    Dalle(summarized_story)
+    text_thread = threading.Thread(target=speak_text, args=(story,))
+    text_thread.start()
+    dis_story(summarized_story)
+    text_thread.join()
